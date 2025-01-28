79469bf5120d084f041ccb49ba4752ab13cef219
Cody Sorgenfrey <codysorgenfrey@gmail.com>
2025-01-27 16:19:26 -0800

    forgot event

--- src/select/template.ts
+++ src/select/template.ts
@@ -4,5 +4,6 @@ import { Select } from './select.js';
 export const template = html<Select>` <select
     part="control"
     ?disabled="${(x) => x.disabled}"
+    @change="${(x, c) => x.handleSelectChange(c.event)}"
   ></select>
   <slot ${slotted('slottedNodes')}></slot>`;
